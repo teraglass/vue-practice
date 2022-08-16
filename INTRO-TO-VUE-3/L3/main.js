a7d95a1a662132da35aad9856ddbc6b77b7f7842
teraglass <67316314+teraglass@users.noreply.github.com>
2022-08-16 18:59:56 +0900

    intro-to-vue3 L2,L3,L4

---
+++ INTRO-TO-VUE-3/L3/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      description: "Soft and Fluffy Socks",
+      url: "https://www.mysockfactory.com/en/plain-women-socks/57-plain-green-socks.html",
+    };
+  },
+});
